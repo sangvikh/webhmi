2d4f96aed931986d719eef2db14d762bb37cf07c
Harald Sangvik <harald.sangvik@gmail.com>
2023-04-19 23:54:23 +0200

    Replaced by code generated by chatGPT

--- static/joystick.js
+++ static/joystick.js
@@ -1,103 +1,100 @@
-class JoystickController
-{
-  constructor( stickID, maxDistance, deadzone )
-  {
-    this.id = stickID;
-    let stick = document.getElementById(stickID);
-    // location from which drag begins, used to calculate offsets
-    this.dragStart = null;
-    // track touch identifier in case multiple joysticks present
-    this.touchId = null;
-    
-    this.active = false;
-    this.value = { x: 0, y: 0 }; 
-    let self = this;
-    function handleDown(event)
-    {
-        self.active = true;
-      // all drag movements are instantaneous
-      stick.style.transition = '0s';
-      // touch event fired before mouse event; prevent redundant mouse event from firing
-      event.preventDefault();
-        if (event.changedTouches)
-          self.dragStart = { x: event.changedTouches[0].clientX, y: event.changedTouches[0].clientY };
-        else
-          self.dragStart = { x: event.clientX, y: event.clientY };
-      // if this is a touch event, keep track of which one
-        if (event.changedTouches)
-          self.touchId = event.changedTouches[0].identifier;
-    }
-    
-    function handleMove(event) 
-    {
-        if ( !self.active ) return;
-        // if this is a touch event, make sure it is the right one
-        // also handle multiple simultaneous touchmove events
-        let touchmoveId = null;
-        if (event.changedTouches)
-        {
-          for (let i = 0; i < event.changedTouches.length; i++)
-          {
-            if (self.touchId == event.changedTouches[i].identifier)
-            {
-              touchmoveId = i;
-              event.clientX = event.changedTouches[i].clientX;
-              event.clientY = event.changedTouches[i].clientY;
-            }
-          }
-          if (touchmoveId == null) return;
-        }
-        const xDiff = event.clientX - self.dragStart.x;
-        const yDiff = event.clientY - self.dragStart.y;
-        const angle = Math.atan2(yDiff, xDiff);
-      const distance = Math.min(maxDistance, Math.hypot(xDiff, yDiff));
-      const xPosition = distance * Math.cos(angle);
-      const yPosition = distance * Math.sin(angle);
-      // move stick image to new position
-      stick.style.transform = `translate3d(${xPosition}%, ${yPosition}%, 0px)`;
-      // deadzone adjustment
-      const distance2 = (distance < deadzone) ? 0 : maxDistance / (maxDistance - deadzone) * (distance - deadzone);
-        const xPosition2 = distance2 * Math.cos(angle);
-      const yPosition2 = distance2 * Math.sin(angle);
-        const xPercent = parseFloat((xPosition2 / maxDistance).toFixed(4));
-        const yPercent = parseFloat((yPosition2 / maxDistance).toFixed(4));
-        
-        self.value = { x: xPercent, y: yPercent };
-        
-      // move stick image to new position
-      //stick.style.transform = `translate3d(${xPercent}%, ${yPercent}%, 0px)`;
-      }
-    function handleUp(event) 
-    {
-        if ( !self.active ) return;
-        // if this is a touch event, make sure it is the right one
-        if (event.changedTouches && self.touchId != event.changedTouches[0].identifier) return;
-        // transition the joystick position back to center
-        stick.style.transition = '.2s';
-        stick.style.transform = `translate3d(0px, 0px, 0px)`;
-        stick.style.scale = `100%`
-        // reset everything
-        self.value = { x: 0, y: 0 };
-        self.touchId = null;
-        self.active = false;
-    }
-    stick.addEventListener('mousedown', handleDown);
-    stick.addEventListener('touchstart', handleDown);
-    document.addEventListener('mousemove', handleMove, {passive: false});
-    document.addEventListener('touchmove', handleMove, {passive: false});
-    document.addEventListener('mouseup', handleUp);
-    document.addEventListener('touchend', handleUp);
-  }
+const joystick = document.getElementById('joystick');
+const joystickArea = document.getElementById('joystickArea');
+const joystickValue = document.getElementById('joystickValue');
+
+// Touch events
+joystickArea.addEventListener('touchstart', handleTouchStart, false);
+joystickArea.addEventListener('touchmove', handleTouchMove, false);
+joystickArea.addEventListener('touchend', handleTouchEnd, false);
+
+// Mouse events
+joystickArea.addEventListener('mousedown', handleMouseDown, false);
+joystickArea.addEventListener('mousemove', handleMouseMove, false);
+joystickArea.addEventListener('mouseup', handleMouseUp, false);
+joystickArea.addEventListener('mouseleave', handleMouseUp, false);
+
+let isMouseDown = false;
+
+function handleTouchStart(e) {
+    e.preventDefault();
+    moveJoystick(e.targetTouches[0]);
 }
 
-let myStick = new JoystickController("joystick", 50, 8);
-function update()
-{
-  document.getElementById("status").innerText = "Joystick1: " + JSON.stringify(joystick1.value);
+function handleTouchMove(e) {
+    e.preventDefault();
+    moveJoystick(e.targetTouches[0]);
 }
-function loop()
-{
-  requestAnimationFrame(loop);
-  update();
+
+function handleTouchEnd(e) {
+    e.preventDefault();
+    joystick.style.transform = `translate(0%, 0%)`;
 }
-loop();
+
+function handleMouseDown(e) {
+    e.preventDefault();
+    isMouseDown = true;
+    moveJoystick(e);
+}
+
+function handleMouseMove(e) {
+    e.preventDefault();
+    if (isMouseDown) {
+        moveJoystick(e);
+    }
+}
+
+function handleMouseUp(e) {
+    e.preventDefault();
+    isMouseDown = false;
+    joystick.style.transform = `translate(0%, 0%)`;
+}
+
+function moveJoystick(input) {
+    const rect = joystickArea.getBoundingClientRect();
+    const centerX = rect.left + rect.width / 2;
+    const centerY = rect.top + rect.height / 2;
+    const inputX = input.clientX;
+    const inputY = input.clientY;
+
+    const deltaX = inputX - centerX;
+    const deltaY = inputY - centerY;
+    const distance = Math.sqrt(deltaX * deltaX + deltaY * deltaY);
+    const maxDistance = rect.width / 2 - joystick.clientWidth / 2;
+
+    const ratio = Math.min(distance, maxDistance) / maxDistance;
+    const angle = Math.atan2(deltaY, deltaX);
+
+    const joystickX = ratio * maxDistance * Math.cos(angle);
+    const joystickY = ratio * maxDistance * Math.sin(angle);
+
+    joystick.style.transform = `translate(${joystickX}px, ${joystickY}px)`;
+
+    // Update the displayed joystick values
+    updateJoystickValue(joystickX / maxDistance, joystickY / maxDistance);
+
+    // Send joystick data to the server
+    sendJoystickData(joystickX / maxDistance, joystickY / maxDistance);
+}
+
+function updateJoystickValue(x, y) {
+    joystickValue.textContent = `x: ${x.toFixed(2)}, y: ${y.toFixed(2)}`;
+}
+
+function sendJoystickData(x, y) {
+  fetch('/joystick-data', {
+      method: 'POST',
+      headers: {
+          'Content-Type': 'application/json'
+      },
+      body: JSON.stringify({x: x, y: y})
+  })
+  .then(response => response.json())
+  .then(data => {
+      if (data.result !== 'success') {
+          console.error('Error sending joystick data');
+      }
+  })
+  .catch(error => {
+      console.error('Error:', error);
+  });
+}
