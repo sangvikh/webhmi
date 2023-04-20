844edba5cc9dce575a612a28ea178228acbaf64e
Harald Sangvik <harald.sangvik@gmail.com>
2023-04-20 23:53:11 +0200

    Fixed some stuff with the battery indicator

--- static/battery.js
+++ static/battery.js
@@ -1,24 +1,29 @@
-function updateBatteryStatus(battery) {
-    const batteryLevel = document.getElementById('battery-level');
-    const batteryText = document.getElementById('battery-text');
+if ('getBattery' in navigator or true) {
+    function updateBatteryStatus(battery) {
+        const batteryLevel = document.getElementById('battery-level');
+        const batteryText = document.getElementById('battery-text');
 
-    const level = battery.level * 100;
-    batteryLevel.style.width = `${level}%`;
-    batteryText.innerText = `${level.toFixed(0)}%`;
+        const level = battery.level * 100;
+        batteryLevel.style.width = `${level}%`;
+        batteryText.innerText = `${level.toFixed(0)}%`;
 
-    if (level > 70) {
-        batteryLevel.style.backgroundColor = 'lime';
-    } else if (level > 30) {
-        batteryLevel.style.backgroundColor = 'yellow';
-    } else {
-        batteryLevel.style.backgroundColor = 'red';
+        if (level > 70) {
+            batteryLevel.style.backgroundColor = rgb(0,200,0);
+        } else if (level > 30) {
+            batteryLevel.style.backgroundColor = rgb(200,200,0);
+        } else {
+            batteryLevel.style.backgroundColor = rgb(200,0,0);
+        }
     }
-}
 
-navigator.getBattery().then(battery => {
-    updateBatteryStatus(battery);
-
-    battery.addEventListener('levelchange', () => {
+    navigator.getBattery().then(battery => {
         updateBatteryStatus(battery);
+
+        battery.addEventListener('levelchange', () => {
+            updateBatteryStatus(battery);
+        });
     });
-});
+} else {
+    const batteryContainer = document.getElementById('battery-container');
+    batteryContainer.style.display = 'none';
+}
