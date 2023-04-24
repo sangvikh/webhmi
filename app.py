9df1a7029ae9801ca942e97284244878613c5a1a
Harald Sangvik <harald.sangvik@gmail.com>
2023-04-24 23:48:40 +0200

    Added video streaming with opencv, made video expand the video-widget

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
-from flask import Flask, render_template, request, jsonify
+import cv2
+from flask import Flask, render_template, request, jsonify, Response
 
 app = Flask(__name__)
 
@@ -16,6 +17,23 @@ def joystick_data():
     print("x: {}, y: {}".format(x, y))
     
     return jsonify({'result': 'success'})
+
+def gen_frames():
+    cap = cv2.VideoCapture(0)
+    while True:
+        success, frame = cap.read()
+        if not success:
+            break
+        else:
+            ret, buffer = cv2.imencode('.jpg', frame)
+            frame = buffer.tobytes()
+            yield (b'--frame\r\n'
+                   b'Content-Type: image/jpeg\r\n\r\n' + frame + b'\r\n')
+
+@app.route('/video_feed')
+def video_feed():
+    return Response(gen_frames(),
+                    mimetype='multipart/x-mixed-replace; boundary=frame')
 
 if __name__ == '__main__':
     app.run(debug=False, port=5000, host="0.0.0.0")
--- readme.md
+++ readme.md
@@ -8,12 +8,13 @@ Web based touch HMI to control different robots
 - Python3: `sudo apt install python3`
 - Pip: `sudo apt install python3-pip`
 - Flask: `pip install Flask `
+- Opencv: `sudo apt install python3-opencv`
 
 ## Usage
     python3 app.py
 
 ## TODO
-- Add video feed
+- Add video feed [x]
 - Add buttons
 - Make widgets movable in runtime
 - Add indicators for battery and wifi signal
